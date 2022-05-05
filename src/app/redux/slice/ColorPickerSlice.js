520c4371527a1fa079db159160816f8018ce547c
Rohan Somai <rohansomai.softvan@gmail.com>
2022-05-05 16:00:56 +0530

    Code till 4 may 2022

---
+++ src/app/redux/slice/ColorPickerSlice.js
@@ -0,0 +1,25 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+export const ColorPickerSlice = createSlice({
+  name: 'ColorPickerSlice',
+  initialState: {
+    title: [],
+    subtitle: [],
+    series: [],
+    legend: [],
+  },
+  reducers: {
+    setRecentColorsForColorPicker: (state, {payload}) => {
+      console.log('setRecentColorsForColorPicker=> payload: ', payload)
+      console.log("state[payload.type] ", JSON.stringify(state))
+      // #TODO dont add duplicate colors
+      // if (!state[payload.type].contains(payload.color))
+      state[payload.type].push(payload.color)
+    },
+  },
+})
+
+
+// Action creators are generated for each case reducer function
+export const {setRecentColorsForColorPicker} = ColorPickerSlice.actions
+export default ColorPickerSlice.reducer
